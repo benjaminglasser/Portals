3c8ba95f4eb9dce6b754e2a14dfb0c2aa2061642
Benjamin Glasser <glasserben@gmail.com>
2022-11-29 21:36:59 +0100

    current updates 11-29-22

--- Assets/Scripts/PortalCamara.cs
+++ Assets/Scripts/PortalCamara.cs
@@ -9,16 +9,23 @@ public class PortalCamara : MonoBehaviour
     public Transform portal;
     public Transform otherPortal;
 
-    void LateUpdate()
+    void Update()
     {
         Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
         transform.position = portal.position + playerOffsetFromPortal;
 
         float angularDifferenceBetweenPortalRotations = Quaternion.Angle(portal.rotation, otherPortal.rotation);
 
+        Vector3 rot = transform.rotation.eulerAngles;
         Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations, Vector3.up);
         Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
-        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+        Vector3 newRot = Quaternion.LookRotation(newCameraDirection, Vector3.up).eulerAngles;
+
+        rot = new Vector3(newRot.x, newRot.y, newRot.z);
+        
+        transform.rotation = Quaternion.Euler(newRot.x, newRot.y, 0f);
+
+
 
     }
 
--- Assets/Scripts/characterBind.cs
+++ Assets/Scripts/characterBind.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class characterBind : MonoBehaviour
 {
 
-    public GameObject character;
+    public GameObject cam;
+    public GameObject parentCam;
     // Start is called before the first frame update
 
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.position = character.transform.position -new Vector3(0,0.5f,0);
-        gameObject.transform.rotation = character.transform.rotation;
+        gameObject.transform.position = cam.transform.position -new Vector3(0,0.5f,0);
+
+        // Vector3 camRot = parentCam.transform.localEulerAngles; 
+        // Vector3 rot = gameObject.transform.localEulerAngles; 
+
+        // Debug.Log(camRot.y);   
+
+        // Quaternion rot = transform.rotation; 
+        // rot = new Vector3 (camRot.y, camRot.y, camRot.y);
+        // gameObject.transform.rotation.y = character.transform.rotation.y;
     }
 }
